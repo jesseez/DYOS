4445ef526d9954440a595834e628065eee89a115
Jessee Zhang <jesseelzhang@gmail.com>
2024-05-04 23:59:17 -0700

    Adds skeleton components for all of the top level pages

--- web/src/index.js
+++ web/src/index.js
@@ -5,6 +5,10 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import Contact from './components/contact/Contact';
+import Schedule from './components/schedule/Schedule';
+import About from './components/about/About';
+import Health from './components/health/Health';
+import CodeOfConduct from './components/codeofconduct/CodeOfConduct';
 import Home from './components/home/Home';
 import NavBar from './components/navbar/NavBar';
 import './index.css';
@@ -17,7 +21,23 @@ const router = createHashRouter([
   {
     path: "/contact",
     element: <Contact />,
-  }
+  },
+  {
+    path: "/schedule",
+    element: <Schedule />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/health",
+    element: <Health />,
+  },
+  {
+    path: "/code",
+    element: <CodeOfConduct />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
