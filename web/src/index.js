3ed09e196f0a9d727224894b8e55670ee8ece116
Jessee Zhang <jesseelzhang@gmail.com>
2024-05-04 20:39:25 -0700

    Fixes issue with initial router set up and modifies skeleton structure

--- web/src/index.js
+++ web/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
+import Contact from './components/contact/Contact';
+import Home from './components/home/Home';
+import NavBar from './components/navbar/NavBar';
 import reportWebVitals from './reportWebVitals';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
